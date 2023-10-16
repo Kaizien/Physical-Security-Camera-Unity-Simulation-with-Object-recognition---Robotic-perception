c34025dd6d2315c189e430c5e653fd0ee367c62c
Grant Hinkle <gdh55@cornell.edu>
2023-10-16 16:56:43 -0400

    changed obstical shape, added some scripting stuff to draw the current vals of the sensor, etc.

---
+++ Assets/ValueFinder.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using TMPro;
+
+public class ValueFinder : MonoBehaviour
+{
+    //G: add TMP game objects for each value
+    public TextMeshProUGUI tiltAngleValue;
+    
+    
+    
+    //This is where we pull the current values from
+    public GameObject panTiltCamera;
+   
+    
+    
+    private PanTiltController sourceScript;
+
+    public float test_tilt_angleValue = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sourceScript = panTiltCamera.GetComponent<PanTiltController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        test_tilt_angleValue = sourceScript.currentTiltAngle;
+        tiltAngleValue.text = test_tilt_angleValue.ToString();
+
+
+    }
+}
