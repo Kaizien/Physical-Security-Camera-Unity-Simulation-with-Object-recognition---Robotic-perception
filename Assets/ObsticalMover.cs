64ce20c89e09a37b5c9ffa6502a31a121d74a8bf
Grant Hinkle <grantdhinkle@gmail.com>
2023-12-07 10:19:25 -0500

    ready for final video

---
+++ Assets/ObsticalMover.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticalMover : MonoBehaviour
+{
+    public Vector3[] waypoints; //the list of waypoints the target will move between
+    public float moveDuration = 3.0f; //duration of time (s) the move between waypoints points
+    public float waitDuration = 1.0f;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MoveThroughPositions(gameObject));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator MoveToPosition(GameObject obj, Vector3 targetPosition, float duration)
+    {
+        Vector3 startingPosition = obj.transform.position;
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            obj.transform.position = Vector3.Lerp(startingPosition, targetPosition, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        obj.transform.position = targetPosition; // Ensure the final position is exact
+    }
+    
+
+    public IEnumerator MoveThroughPositions(GameObject obj)
+    {
+        int currentPos = 0;
+
+        while (true) // Infinite loop
+        {
+            Vector3 nextPosition = waypoints[currentPos];
+            yield return MoveToPosition(obj, nextPosition, moveDuration);
+            yield return new WaitForSeconds(waitDuration);
+
+            currentPos = (currentPos + 1) % waypoints.Length; // Move to the next position, loop back to 0 if we're at the end
+        }
+    }
+}
